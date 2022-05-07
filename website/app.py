8bb108f047b707b8e200bf4e928a66f96108390a
awu2003 <wualice2003@gmail.com>
2022-05-07 00:01:09 -0400

    made app edits

--- website/app.py
+++ website/app.py
@@ -128,21 +128,89 @@ def edit():
 @login_required
 def edit_header():
     if request.method == "GET":
+        user_id = session["user_id"]
+        # gets segment number to do conditional rendering on the html
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
         return render_template("edit-header.html")
+    else:
+        # get text from form
+        header_text = request.form.get("header-text")
+        if header_text == "":
+            return apology("must enter text")
+        
+        # SQL time!
+        user_id = session["user_id"]
+        # update segments number for users table
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
+        db.execute("UPDATE users SET segments_number = ? WHERE user_id = ?", segments_number + 1, user_id)
+        
+        # add segment to segments table
+        db.execute("INSERT INTO segments (user_id, segment_type, location) VALUES (?, ?, ?)",
+                   user_id, "header", segments_number + 1)
+        # add segment to header table
+        segment_id = db.execute("SELECT segment_id FROM segments WHERE user_id = ? AND location = ?", user_id, segments_number + 1)[0]['segment_id']
+        db.execute("INSERT INTO header (header_text, segment_id) VALUES (?, ?)",
+                   header_text, segment_id)
+        return redirect("/")
 
 @app.route("/edit-paragraph", methods=["GET", "POST"])
 @login_required
 def edit_paragraph():
     if request.method == "GET":
+        user_id = session["user_id"]
+        # gets segment number to do conditional rendering on the html
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
         return render_template("edit-paragraph.html")
-    #else:
+    else:
+        # get text from form
+        paragraph_text = request.form.get("paragraph-text")
+        if paragraph_text == "":
+            return apology("must enter text")
+        
+        # SQL time!
+        user_id = session["user_id"]
+        # update segments number for users table
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
+        db.execute("UPDATE users SET segments_number = ? WHERE user_id = ?", segments_number + 1, user_id)
+        
+        # add segment to segments table
+        db.execute("INSERT INTO segments (user_id, segment_type, location) VALUES (?, ?, ?)",
+                   user_id, "paragraph", segments_number + 1)
+        # add segment to paragraph table
+        segment_id = db.execute("SELECT segment_id FROM segments WHERE user_id = ? AND location = ?", user_id, segments_number + 1)[0]['segment_id']
+        db.execute("INSERT INTO paragraph (paragraph_text, segment_id) VALUES (?, ?)",
+                   paragraph_text, segment_id)
+        return redirect("/")
 
 
 @app.route("/edit-image", methods=["GET", "POST"])
 @login_required
 def edit_image():
     if request.method == "GET":
+        user_id = session["user_id"]
+        # gets segment number to do conditional rendering on the html
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
         return render_template("edit-image.html")
+    else:
+        # get text from form
+        image_url = request.form.get("image-url")
+        if image_url == "":
+            return apology("must enter url")
+        
+        # SQL time!
+        user_id = session["user_id"]
+        # update segments number for users table
+        segments_number = db.execute("SELECT segments_number FROM users WHERE user_id = ?", user_id)[0]['segments_number']
+        db.execute("UPDATE users SET segments_number = ? WHERE user_id = ?", segments_number + 1, user_id)
+        
+        # add segment to segments table
+        db.execute("INSERT INTO segments (user_id, segment_type, location) VALUES (?, ?, ?)",
+                   user_id, "image", segments_number + 1)
+        # add segment to image table
+        segment_id = db.execute("SELECT segment_id FROM segments WHERE user_id = ? AND location = ?", user_id, segments_number + 1)[0]['segment_id']
+        db.execute("INSERT INTO image (image_url, segment_id) VALUES (?, ?)",
+                   image_url, segment_id)
+        return redirect("/")
 
 @app.route("/about", methods=["GET"])
 @login_required
